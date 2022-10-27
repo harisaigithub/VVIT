e99b850eb2d4c1564382c64087a4817e880fb836
harisaigithub <harisaiparasa@gmail.com>
2022-10-27 22:38:42 +0530

    ds lab

--- LABS/DS LAB/exp 4 Mergesort.py
+++ LABS/DS LAB/exp 4 Mergesort.py
@@ -1,47 +1,46 @@
-def merge (arr, l, m, r):
-    n1 = m - l + 1
-    n2 = r - m
-    l = [0] * (n1)
-    r = [0] * (n2)
-    for i in range (0, n1):
-        l[i] = arr[1+i]
-    for j in range(0, n2):
-        r[j] = arr[m + i+ j]
-        i = 0
-        j = 0
-        k = l
-        while(i<n1 and j<n2):
-            if l [i] <= r[j]:
-                arr[k] = l[j]
+def mergeSort(arr):
+    if len(arr) > 1:
+ 
+        mid = len(arr)//2
+ 
+        L = arr[:mid]
+ 
+        R = arr[mid:]
+ 
+        mergeSort(L)
+ 
+        mergeSort(R)
+ 
+        i = j = k = 0
+ 
+        while i < len(L) and j < len(R):
+            if L[i] <= R[j]:
+                arr[k] = L[i]
                 i += 1
             else:
-               arr[k] = r[j]
-               j += 1
-
+                arr[k] = R[j]
+                j += 1
             k += 1
-        while i<n1 :
-            arr[k] = l[i]
+ 
+        while i < len(L):
+            arr[k] = L[i]
             i += 1
             k += 1
-            while j<n2:
-                arr[k] = r[j]
-                j += 1
-                k += 1
-def mergesort(arr, l, r):
-    if l < r:
-        m = l + (r - 1)//2
-    mergesort(arr, l, m)
-    mergesort(arr, m+1, r)
-    mergesort(arr, l, m, r)
-arr=[]
-n = int(input())
-print ("ENTER List ELEMENT: ")
-for i in range(n):
-      ele = int(input())
-      arr.append(ele)
-for i in range(n):
-      print("%d" % arr[i], end = " ")
-mergesort(arr, 0, n-1)
-print ("\n sorted array is ")
-for i in range (n):
-                print ("%d" % arr[i], end = " ")
+ 
+        while j < len(R):
+            arr[k] = R[j]
+            j += 1
+            k += 1
+ 
+def printList(arr):
+    for i in range(len(arr)):
+        print(arr[i], end=" ")
+    print()
+ 
+if __name__ == '__main__':
+    arr = [12, 11, 13, 5, 6, 7]
+    print("Given array is", end="\n")
+    printList(arr)
+    mergeSort(arr)
+    print("Sorted array is: ", end="\n")
+    printList(arr)
